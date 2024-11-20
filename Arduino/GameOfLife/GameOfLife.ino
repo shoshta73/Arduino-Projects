ccaf6a4eea20c40d5e0dee1f016a0dace66b4a9e
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-20 19:43:59 +0100

    use 2d arrays

--- Arduino/GameOfLife/GameOfLife.ino
+++ Arduino/GameOfLife/GameOfLife.ino
@@ -3,13 +3,12 @@
 #define ROWS 8
 #define COLS 12
 
-uint8_t grid[ROWS][COLS];
+uint8_t current_grid[ROWS][COLS] = { 0 };
 
-uint8_t current_grid[ROWS * COLS] = { 0 };
-uint8_t next_grid[ROWS * COLS] = { 0 };
+uint8_t next_grid[ROWS][COLS] = { 0 };
 
-uint8_t *current = current_grid;
-uint8_t *next = next_grid;
+uint8_t (*current)[COLS] = current_grid;
+uint8_t (*next)[COLS] = next_grid;
 
 ArduinoLEDMatrix matrix;
 
@@ -20,7 +19,7 @@ void setup() {
   for (int x = 0; x < COLS; x++) {
     for (int y = 0; y < ROWS; y++) {
       if (random(0, 100) < 30) {
-        current_grid[y * ROWS + x] = 1;
+        current_grid[y][x] = 1;
       }
     }
   }
@@ -41,38 +40,38 @@ void loop() {
       int top_y = y ? y - 1 : ROWS - 1;
       int bottom_y = (y != ROWS - 1) ? y + 1 : 0;
 
-      if (current[y * COLS + left_x])
+      if (current[y][left_x] == 1)
         alive++;
-      if (current[top_y * COLS + left_x])
+      if (current[top_y][left_x] == 1)
         alive++;
-      if (current[top_y * COLS + x])
+      if (current[top_y][x] == 1)
         alive++;
-      if (current[top_y * COLS + right_x])
+      if (current[top_y][right_x] == 1)
         alive++;
-      if (current[y * COLS + right_x])
+      if (current[y][right_x] == 1)
         alive++;
-      if (current[bottom_y * COLS + right_x])
+      if (current[bottom_y][right_x] == 1)
         alive++;
-      if (current[bottom_y * COLS + x])
+      if (current[bottom_y][x] == 1)
         alive++;
-      if (current[bottom_y * COLS + left_x])
+      if (current[bottom_y][left_x] == 1)
         alive++;
 
-      if (current[y * COLS + x]) {
+      if (current[y][x] == 1) {
         if (alive < 2 || alive > 3)
-          next[y * COLS + x] = false;
+          next[y][x] = 0;
         else
-          next[y * COLS + x] = true;
+          next[y][x] = 1;
       } else {
         if (alive == 3)
-          next[y * COLS + x] = true;
+          next[y][x] = 1;
         else
-          next[y * COLS + x] = false;
+          next[y][x] = 0;
       }
     }
   }
 
-  uint8_t *tmp = current;
+  uint8_t(*tmp)[COLS] = current;
   current = next;
   next = tmp;
 
@@ -80,10 +79,5 @@ void loop() {
 }
 
 void display_grid(void) {
-  for (int x = 0; x < COLS; x++)
-    for (int y = 0; y < ROWS; y++)
-      grid[y][x] = current[y * ROWS + x];
-
-
-  matrix.renderBitmap(grid, 8, 12);
+  matrix.renderBitmap(current, 8, 12);
 }
--- DesktopDemos/GameOfLife/src/main.cc
+++ DesktopDemos/GameOfLife/src/main.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdint>
 
 #include <raylib.h>
 
@@ -7,22 +7,26 @@
 #define WIDTH 100
 #define HEIGHT WIDTH
 
+uint8_t current_grid[ROWS][COLS] = { 0 };
+
+uint8_t next_grid[ROWS][COLS] = { 0 };
+
+uint8_t (*current)[COLS] = current_grid;
+uint8_t (*next)[COLS] = next_grid;
+
 int
 main(void)
 {
-	bool* current = new bool[ROWS * COLS];
-	bool* next = new bool[ROWS * COLS];
-
 	for (int x = 0; x < COLS; x++) {
 		for (int y = 0; y < ROWS; y++) {
-			current[y * COLS + x] = GetRandomValue(0, 10) < 3 ? true : false;
-			next[y * COLS + x] = false;
+			current_grid[y][x] = GetRandomValue(0, 10) < 3 ? 1 : 0;
+			next_grid[y][x] = false;
 		}
 	}
 
 	InitWindow(COLS * WIDTH, ROWS * HEIGHT, "GameOfLife");
 
-	SetTargetFPS(10);
+	SetTargetFPS(5);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
@@ -31,7 +35,7 @@ main(void)
 
 		for (int x = 0; x < COLS; x++) {
 			for (int y = 0; y < ROWS; y++) {
-				bool cell = current[y * COLS + x];
+				uint8_t cell = current[y][x];
 
 				if (cell)
 					DrawRectangle(x * WIDTH, y * HEIGHT, WIDTH, HEIGHT, RED);
@@ -43,38 +47,38 @@ main(void)
 				int top_y = y ? y - 1 : ROWS - 1;
 				int bottom_y = (y != ROWS - 1) ? y + 1 : 0;
 
-				if (current[y * COLS + left_x])
+				if (current[y][left_x] == 1)
 					alive++;
-				if (current[top_y * COLS + left_x])
+				if (current[top_y][left_x] == 1)
 					alive++;
-				if (current[top_y * COLS + x])
+				if (current[top_y][x] == 1)
 					alive++;
-				if (current[top_y * COLS + right_x])
+				if (current[top_y][right_x] == 1)
 					alive++;
-				if (current[y * COLS + right_x])
+				if (current[y][right_x] == 1)
 					alive++;
-				if (current[bottom_y * COLS + right_x])
+				if (current[bottom_y][right_x] == 1)
 					alive++;
-				if (current[bottom_y * COLS + x])
+				if (current[bottom_y][x] == 1)
 					alive++;
-				if (current[bottom_y * COLS + left_x])
+				if (current[bottom_y][left_x] == 1)
 					alive++;
 
-				if (cell) {
+				if (cell == 1) {
 					if (alive < 2 || alive > 3)
-						next[y * COLS + x] = false;
+						next[y][x] = false;
 					else
-						next[y * COLS + x] = true;
+						next[y][x] = true;
 				} else {
 					if (alive == 3)
-						next[y * COLS + x] = true;
+						next[y][x] = true;
 					else
-						next[y * COLS + x] = false;
+						next[y][x] = false;
 				}
 			}
 		}
 
-		bool* tmp = current;
+		uint8_t(*tmp)[COLS] = current;
 		current = next;
 		next = tmp;
 
@@ -88,9 +92,6 @@ main(void)
 	}
 
 	CloseWindow();
-
-	delete[] current;
-	delete[] next;
 
 	return 0;
 }
