53d43279cda95142ffe068ec823bba6aab18f27e
Borna Šoštarić <sostaric.borna@hotmail.com>
2024-11-20 12:28:34 +0100

    draw grid

--- GameOfLife/src/main.cc
+++ GameOfLife/src/main.cc
@@ -1,21 +1,27 @@
 #include <raylib.h>
 
+#define ROWS 8
+#define COLS 12
+#define WIDTH 100
+#define HEIGHT WIDTH
+
 int
 main(void)
 {
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-
-	InitWindow(screenWidth, screenHeight, "GameOfLife");
+	InitWindow(COLS * WIDTH, ROWS * HEIGHT, "GameOfLife");
 
 	SetTargetFPS(60);
 
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 
-		ClearBackground(RAYWHITE);
+		ClearBackground(WHITE);
 
-		DrawText("GameOfLife", 190, 200, 20, LIGHTGRAY);
+		for (int x = 0; x < COLS - 1; x++)
+			DrawLine(x * WIDTH + WIDTH + 1, 0, x * WIDTH + WIDTH + 1, ROWS * HEIGHT, BLACK);
+
+		for (int y = 0; y < ROWS - 1; y++)
+			DrawLine(0, y * HEIGHT + HEIGHT + 1, COLS * WIDTH, y * HEIGHT + HEIGHT + 1, BLACK);
 
 		EndDrawing();
 	}
